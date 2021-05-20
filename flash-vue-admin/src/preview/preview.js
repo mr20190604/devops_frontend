963d405ec532ba2ba57b2ec183b943e15c957d87
jinchengcheng <4429619@qq.com>
2021-05-20 18:42:16 +0800

    增加预览对应的组件

---
+++ flash-vue-admin/src/preview/preview.js
@@ -0,0 +1,36 @@
+import request from '@/utils/request'
+import { getApiUrl } from '@/utils/utils'
+
+const Base64 = require('js-base64').Base64
+
+
+
+/**
+ *  url 不带参数url
+ *  param   {id:12,name:13}
+ *  let params = {idFile:id, fileName:fileName};
+    preview('/file/download', params, fileName);
+ * */
+export function downloadFile(url, param, fileName) {
+  getFileStream(url, param).then(response=>{
+    let blob = new Blob([response]);
+    let downloadElement = document.createElement("a");
+    let href = window.URL.createObjectURL(blob);//创建下载的链接
+    downloadElement.href = href;
+    downloadElement.download = fileName;//下载后文件名
+    document.body.appendChild(downloadElement);
+    downloadElement.click();//点击下载
+    document.body.removeChild(downloadElement); //下载完成移除元素
+    window.URL.revokeObjectURL(href); //释放掉blob对象
+    this.$message.success(`操作成功`);
+  });
+}
+
+export function getFileStream(url, params) {
+  return request({
+    url: url,
+    method: 'get',
+    params
+  })
+}
+
---
+++ flash-vue-admin/src/preview/preview.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <iframe v-if="fileType === 1"  :style="previewStyle" :src="previewFileUrl"></iframe>
+    <img v-if="fileType === 2" style="previewStyle" :src="previewFileUrl"/>
+    <video v-if="fileType === 3"  style="previewStyle" src="previewFileUrl"></video>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "preview",
+        props:['previewFileUrl','previewStyle','fileType'],
+        data() {
+          return {
+
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
