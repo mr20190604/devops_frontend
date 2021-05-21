62d2402c3f9e65ce472de5576379b7cf231f12cd
jinchengcheng <4429619@qq.com>
2021-05-21 09:29:48 +0800

    添加预览地址全局配置

--- flash-vue-admin/src/utils/utils.js
+++ flash-vue-admin/src/utils/utils.js
@@ -1,4 +1,21 @@
 const apiUrl = process.env.VUE_APP_BASE_API
+const previewUrl_1 = process.env.VUE_APP_BASE_PREVIEW_1
+const previewUrl_2 = process.env.VUE_APP_BASE_PREVIEW_2
+const previewUrl_3 = process.env.VUE_APP_BASE_PREVIEW_3
+
 export function getApiUrl() {
   return apiUrl
+}
+
+export function getPreviewUrl(ct) {
+  switch (ct) {
+    case 1:
+      return previewUrl_1;
+    case 2:
+      return previewUrl_2;
+    case 3:
+      return previewUrl_3;
+    default:
+      return previewUrl_1;
+  }
 }
