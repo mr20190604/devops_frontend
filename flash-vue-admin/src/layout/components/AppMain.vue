19acf11f7043d5188ab103b4512bfd06993c3fb7
jinchengcheng <4429619@qq.com>
2021-05-25 18:00:20 +0800

    去掉备案

--- flash-vue-admin/src/layout/components/AppMain.vue
+++ flash-vue-admin/src/layout/components/AppMain.vue
@@ -10,10 +10,10 @@
     <div class="footer">
       <p>
         <a href="https://www.9-heng.com/" style="color: #0a76a4;" target="_blank">Copyright © 2021  中科九衡科技有限公司</a>
-        <span>版权所有</span>
+        <!--<span>版权所有</span>
         <img src="../../assets/img/备案.png" alt="备案" style="margin-left: 30px;margin-right: 10px;">
         <span>皖公网安备 34019202001488号</span>
-        <span style="margin-left: 30px;">皖ICP备18001116号</span>
+        <span style="margin-left: 30px;">皖ICP备18001116号</span>-->
       </p>
     </div>
   </div>
