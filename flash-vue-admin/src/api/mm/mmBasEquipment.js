54544c7c67f3e6693e64c3c74f35ee0b815fef83
huangwei <huangwei@9-heng.com>
2021-06-04 11:47:14 +0800

    修改问题
    Signed-off-by: huangwei <huangwei@9-heng.com>

--- flash-vue-admin/src/api/mm/mmBasEquipment.js
+++ flash-vue-admin/src/api/mm/mmBasEquipment.js
@@ -1,42 +1,42 @@
 import request from '@/utils/request'
 export default {
-    getList:function(params) {
-        return request({
-            url: '/bas/equipment/list',
-            method: 'get',
-            params
-        })
-    },
-    add:function(params) {
-        return request({
-            url: '/bas/equipment',
-            method: 'post',
-            params
-        })
-    },
-    update:function(params) {
-        return request({
-            url: '/bas/equipment',
-            method: 'PUT',
-            params
-        })
-    },
-    remove:function(id) {
-        return request({
-            url: '/bas/equipment',
-            method: 'delete',
-            params: {
-                id: id
-            }
-        })
-    },
+  getList: function(params) {
+    return request({
+      url: '/bas/equipment/list',
+      method: 'get',
+      params
+    })
+  },
+  add: function(params) {
+    return request({
+      url: '/bas/equipment',
+      method: 'post',
+      params
+    })
+  },
+  update: function(params) {
+    return request({
+      url: '/bas/equipment',
+      method: 'PUT',
+      params
+    })
+  },
+  remove: function(id) {
+    return request({
+      url: '/bas/equipment',
+      method: 'delete',
+      params: {
+        id: id
+      }
+    })
+  },
   queryAll: function(equipmentType) {
     return request({
       url: '/bas/equipment/queryAll',
       method: 'get',
-      params:{
-        equipmentType:equipmentType,
+      params: {
+        equipmentType: equipmentType
       }
     })
-  },
+  }
 }
