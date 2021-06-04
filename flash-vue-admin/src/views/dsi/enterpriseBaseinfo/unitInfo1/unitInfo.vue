67e1722bef41aabe9f1f41ea3ec6b52e4005b31b
caolei <ZKJH@ZKJH-YJY-2>
2021-06-04 12:50:38 +0800

    隐藏时间查询

--- flash-vue-admin/src/views/dsi/enterpriseBaseinfo/unitInfo1/unitInfo.vue
+++ flash-vue-admin/src/views/dsi/enterpriseBaseinfo/unitInfo1/unitInfo.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app-container">
 
-
+    <div class="table-list">
 
     <el-table :data="list" v-loading="listLoading" element-loading-text="Loading" border fit highlight-current-row
               :row-key="row=>row.id"
@@ -38,6 +38,7 @@
       </el-table-column>
 
     </el-table>
+    </div>
     <div style="position:absolute;bottom: 30px;width: 100%">
     <el-pagination
       background
