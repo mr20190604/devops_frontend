b6d0c41cdafaa6dc33d1f83eddc1d49ba2f7af37
514973524@qq.com <514973524@qq.com>
2021-06-10 22:17:32 +0800

    修改登录页样式，修改系统选择效果

--- flash-vue-admin/src/views/login/index.vue
+++ flash-vue-admin/src/views/login/index.vue
@@ -1,74 +1,178 @@
 <template>
-  <div class="login-container" style=" background-image: url(./img/login_bj.png);">
-
-    <el-form
-      ref="loginForm"
-      :model="loginForm"
-      :rules="loginRules"
-      class="login-form"
-      auto-complete="on"
-      label-position="left"
-      style="position: absolute;top: 240px;right: 350px;width: 400px;"
-    >
+  <div
+    class="login-container"
+    style="background-image: url(./img/login_bj.png)"
+  >
+    <div class="login-wrap">
       <div class="title-container">
-        <h3 class="title">{{ $t('login.title') }}</h3>
-        <!--<lang-select class="set-language"/>-->
+        <img src="@/assets/img/login-title.png" alt="" />
       </div>
+      <el-form
+        ref="loginForm"
+        :model="loginForm"
+        :rules="loginRules"
+        class="login-form"
+        auto-complete="on"
+        label-position="left"
+      >
+        <h3>用户登录</h3>
+        <el-form-item prop="username">
+          <span class="icon">
+            <img src="@/assets/img/login-user-icon.png" alt="" />
+          </span>
+          <el-input
+            v-model="loginForm.username"
+            :placeholder="$t('login.username')"
+            name="username"
+            type="text"
+            auto-complete="on"
+          />
+        </el-form-item>
 
-      <el-form-item prop="username" style="background: #001c5a;opacity:0.5;">
-        <span class="svg-container">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input
-          v-model="loginForm.username"
-          :placeholder="$t('login.username')"
-          name="username"
-          type="text"
-          auto-complete="on"
-        />
-      </el-form-item>
-
-      <el-form-item prop="password" style="background: #001c5a;opacity:0.5;">
-        <span class="svg-container">
-          <svg-icon icon-class="password" />
-        </span>
-        <el-input
-          v-model="loginForm.password"
-          :type="pwdType"
-          :placeholder="$t('login.password')"
-          name="password"
-          auto-complete="on"
-          @keyup.enter.native="handleLogin"
-        />
-        <span class="show-pwd" @click="showPwd">
-          <svg-icon icon-class="eye" />
-        </span>
-      </el-form-item>
-      <el-row>
-        <el-col :span="12">
-          <el-checkbox v-model="loginForm.remember" style="color:#1bb7b4;">记住密码</el-checkbox>
-        </el-col>
-        <el-col :span="12" style="text-align: right">
-          <div style="color: #1bb7b4;font-weight: 500;cursor: pointer;font-size: 14px;display: inline-block;padding-right: 1px;line-height: 19px;">
-            忘记密码?</div>
-        </el-col>
-      </el-row>
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">{{ $t('login.logIn') }}</el-button>
-
-      <!--<div style="position:relative;">
-        <div class="tips">
-          <span>{{ $t('login.username') }} : admin</span>
-          <span>{{ $t('login.password') }} : admin</span>
+        <el-form-item prop="password">
+          <span class="icon">
+            <img src="@/assets/img/login-pwd-icon.png" alt="" />
+          </span>
+          <el-input
+            v-model="loginForm.password"
+            :type="pwdType"
+            :placeholder="$t('login.password')"
+            name="password"
+            auto-complete="on"
+            @keyup.enter.native="handleLogin"
+          />
+          <span class="show-pwd" @click="showPwd">
+            <svg-icon icon-class="eye" />
+          </span>
+        </el-form-item>
+        <div class="forget-pwd-wrap">
+          <el-checkbox v-model="loginForm.remember">记住密码</el-checkbox>
+          <p>忘记密码?</p>
         </div>
-        <div class="tips">
-          <span style="margin-right:18px;">{{ $t('login.username') }} : developer</span>
-          <span>{{ $t('login.password') }} : developer</span>
-        </div>
-      </div>-->
-    </el-form>
 
+        <el-button
+          :loading="loading"
+          type="primary"
+          @click.native.prevent="handleLogin"
+          >{{ $t("login.logIn") }}</el-button
+        >
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script src="./login.js"></script>
-<style rel="stylesheet/scss" lang="scss" src="./login.scss"></style>
+<style lang="scss" scoped>
+.login-container {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  .login-wrap {
+    width: 501px;
+    position: absolute;
+    right: 12.6vw;
+    top: 18vh;
+    .title-container {
+      text-align: center;
+      margin-bottom: 36px;
+    }
+    >>> .login-form {
+      width: 501px;
+      height: 498px;
+      background: url("../../assets/img/login-box-bg.png") no-repeat center;
+      background-size: cover;
+      margin: 0 auto;
+      h3 {
+        text-align: center;
+        font-size: 32px;
+        font-weight: normal;
+        color: #00bff4;
+        letter-spacing: 11px;
+        padding-top: 28px;
+        line-height: 102px;
+      }
+      .forget-pwd-wrap {
+        display: flex;
+        align-items: center;
+        width: 449px;
+        justify-content: space-between;
+        margin: 0 auto;
+        p {
+          font-size: 18px;
+          color: #fff;
+        }
+        .el-checkbox {
+          display: flex;
+          align-items: center;
+          .el-checkbox__input {
+            .el-checkbox__inner {
+              border: 3px solid rgb(11, 161, 248);
+              opacity: 0.4;
+              border-radius: 0px;
+              width: 18px;
+              height: 18px;
+              background-color: rgba(6, 135, 205, 0.302);
+            }
+          }
+          .el-checkbox__label {
+            font-size: 18px;
+            color: #fff;
+          }
+        }
+      }
+      .el-button {
+        display: block;
+        background-color: rgb(11, 161, 248);
+        font-size: 32px;
+        letter-spacing: 11px;
+        width: 449px;
+        height: 56px;
+        margin: 24px auto 0;
+      }
+      .el-form-item {
+        display: flex;
+        justify-content: center;
+        background-size: cover;
+        margin-bottom: 24px;
+        .el-form-item__content {
+          line-height: 56px;
+          display: flex;
+          align-items: center;
+          width: 449px;
+          height: 56px;
+          line-height: 56px;
+          background: url("../../assets/img/login-border.png") no-repeat center;
+          background-size: cover;
+          background-color: #fff;
+          .icon {
+            width: 46px;
+            margin-left: 25px;
+            display: flex;
+            align-items: center;
+            flex-shrink: 0;
+          }
+          .el-input__inner {
+            border-radius: 0px;
+            padding-left: 0;
+            height: 54px;
+            line-height: 1;
+            background: #fff;
+            border: none;
+            font-size: 18px;
+            color: #313131;
+          }
+        }
+      }
+    }
+  }
+}
+input:-webkit-autofill{
+   background: none !important;
+box-shadow:0 0 0 1000px white inset !important;
+	-webkit-box-shadow:0 0 0 1000px white inset !important;
+}
+input:-internal-autofill-selected {
+             
+            }
+</style>
+
--- flash-vue-admin/src/views/platform/index.vue
+++ flash-vue-admin/src/views/platform/index.vue
@@ -2,34 +2,48 @@
   <div class="bg">
     <!-- 星星动效 -->
     <vue-particles
-        color="#dedede"
-        :particleOpacity="0.4"
-        :particlesNumber="60"
-        shapeType="star"
-        :particleSize="3"
-        linesColor="#dedede"
-        :linesWidth="1"
-        :lineLinked="false"
-        :lineOpacity="0.4"
-        :linesDistance="150"
-        :moveSpeed="3"
-        :hoverEffect="false"
-        hoverMode="grab"
-        :clickEffect="true"
-        clickMode="push"
-        class="particles"
-      >
-      </vue-particles>
-      <!-- 轮播 -->
-    <div class="recommendPage" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">
+      color="#dedede"
+      :particleOpacity="0.4"
+      :particlesNumber="60"
+      shapeType="star"
+      :particleSize="3"
+      linesColor="#dedede"
+      :linesWidth="1"
+      :lineLinked="false"
+      :lineOpacity="0.4"
+      :linesDistance="150"
+      :moveSpeed="3"
+      :hoverEffect="false"
+      hoverMode="grab"
+      :clickEffect="true"
+      clickMode="push"
+      class="particles"
+    >
+    </vue-particles>
+    <!-- 轮播 -->
+    <div
+      class="recommendPage"
+      @mouseenter="onMouseEnter"
+      @mouseleave="onMouseLeave"
+    >
       <swiper :options="swiperOption" ref="mySwiper">
-        <swiper-slide v-for="(item, index) in swiperData" :key="index" :data-item="index">
+        <swiper-slide
+          v-for="(item, index) in swiperData"
+          :key="index"
+          :data-item="index"
+        >
           <div :style="{ 'background-image': 'url(' + item.img + ')' }"></div>
         </swiper-slide>
       </swiper>
     </div>
-     <div class="swiper-button-prev common-swiper-button el-icon-arrow-left" slot="button-prev" ></div>
-     <div class="swiper-button-next common-swiper-button el-icon-arrow-right" slot="button-next" ></div>
+    <div
+      class="swiper-button-prev common-swiper-button el-icon-arrow-left"
+      slot="button-prev"
+    ></div>
+    <div
+      class="swiper-button-next common-swiper-button el-icon-arrow-right"
+      slot="button-next"
+    ></div>
   </div>
 </template>
 
@@ -41,7 +55,7 @@ import "swiper/dist/css/swiper.css";
 export default {
   components: {
     swiper,
-    swiperSlide
+    swiperSlide,
   },
   data() {
     return {
@@ -49,47 +63,49 @@ export default {
       swiperData: [
         {
           img: "/img/swiper-img1.png",
-          url:'#',
-          title:'大数据分析系统'
+          url: "#",
+          title: "大数据分析系统",
         },
         {
           img: "/img/swiper-img2.png",
-          url:'/enterpriseBaseinfo',
-          title:'数据资源一体系统'
+          url: "/enterpriseBaseinfo",
+          title: "数据资源一体系统",
         },
         {
           img: "/img/swiper-img3.png",
-          url:'#',
-          title:'三维地理信息系统'
+          url: "#",
+          title: "三维地理信息系统",
         },
         {
           img: "/img/swiper-img4.png",
-          url:'#',
-          title:'预测预警子系统'
+          url: "#",
+          title: "预测预警子系统",
         },
         {
           img: "/img/swiper-img5.png",
-          url:'#',
-          title:'数据资源辅助系统'
-        }
+          url: "#",
+          title: "数据资源辅助系统",
+        },
       ],
       swiperOption: {
+        speed: 3000,
         effect: "coverflow",
         grabCursor: true,
         centeredSlides: true,
         slidesPerView: "auto",
         coverflowEffect: {
-          rotate: 50,
-          stretch: 0,
-          depth: 115,
+          rotate: 42,
+          stretch: -40,
+          depth: 200,
           modifier: 1,
-          slideShadows: false
+          slideShadows: false,
         },
+
         loop: true,
         autoplay: {
           delay: 1800,
           stopOnLastSlide: false,
-          disableOnInteraction: false
+          disableOnInteraction: false,
         },
         // 显示分页
         // pagination: {
@@ -99,40 +115,38 @@ export default {
         // 设置点击箭头
         navigation: {
           nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev"
+          prevEl: ".swiper-button-prev",
         },
         on: {
-          click: function() {
-            const index = this.clickedSlide.attributes["data-item"].nodeValue
-          
-            vm.handleClickSlide(index);
-          }
-        }
+          click: function () {
+            const index = this.clickedSlide.attributes["data-item"].nodeValue;
 
-      }
+            vm.handleClickSlide(index);
+          },
+        },
+      },
     };
   },
   computed: {
     swiper() {
       return this.$refs.mySwiper.swiper;
-    }
+    },
   },
-  created(){
+  created() {
     vm = this;
   },
 
-  methods:{
-    handleClickSlide(index){
-       this.$router.push(this.swiperData[index].url)
+  methods: {
+    handleClickSlide(index) {
+      this.$router.push(this.swiperData[index].url);
     },
     onMouseEnter() {
-        this.swiper.autoplay.stop()
+      this.swiper.autoplay.stop();
     },
     onMouseLeave() {
-        this.swiper.autoplay.start()
-    }
-
-  }
+      this.swiper.autoplay.start();
+    },
+  },
 };
 </script>
 <style scoped lang="scss">
@@ -140,9 +154,9 @@ export default {
   width: 100%;
   height: 100%;
   background: url("../../assets/img/bg.png") no-repeat top center;
-  background-size: 100% 1005;
+  background-size: 100% 100%;
   overflow: hidden;
-  .particles{
+  .particles {
     position: fixed;
     width: 100%;
     height: 100%;
@@ -153,53 +167,52 @@ export default {
   }
 
   .common-swiper-button {
-     width: 62px;
+    width: 62px;
     height: 62px;
     background: none;
     border-radius: 50%;
-    border: 5px solid rgba(168,213,232,.27);
+    border: 5px solid rgba(168, 213, 232, 0.27);
     text-align: center;
     line-height: 53px;
     font-size: 30px;
-    color: rgba(168,213,232,.47);
+    color: rgba(168, 213, 232, 0.47);
     font-weight: bold;
     transition: all 0.3s linear;
-      &.swiper-button-next {
-        right: 60px;
-        // background: url("/img/swiper-right-btn.png");
-      }
-      &.swiper-button-prev {
-        left: 60px;
-        // background: url("/img/swiper-left-btn.png");
-      }
-      &:hover{
-        color:rgba(255,255,255,.6);
-        border: 5px solid rgba(255,255,255,.4);
-      }
+    &.swiper-button-next {
+      right: 60px;
+      // background: url("/img/swiper-right-btn.png");
     }
+    &.swiper-button-prev {
+      left: 60px;
+      // background: url("/img/swiper-left-btn.png");
+    }
+    &:hover {
+      color: rgba(255, 255, 255, 0.6);
+      border: 5px solid rgba(255, 255, 255, 0.4);
+    }
+  }
 
-.recommendPage {
-  margin-top: 34.3vh;
-  >>> .swiper-container {
-    position: relative;
-    width: 1280px;
-    height: 375px;
-    .swiper-slide {
-      width: 325px;
-      height: 375px;
-      div {
-        width: 100%;
-        height: 100%;
-        a{
-          display: block;
+  .recommendPage {
+    margin-top: 34.3vh;
+    >>> .swiper-container {
+      position: relative;
+      width: 1265px;
+      height: 370px;
+      .swiper-slide {
+        width: 320px;
+        height: 370px;
+        div {
           width: 100%;
           height: 100%;
+          background-size: 100% 100%;
+          a {
+            display: block;
+            width: 100%;
+            height: 100%;
+          }
         }
       }
-      
     }
-    
   }
-}
 }
 </style>
