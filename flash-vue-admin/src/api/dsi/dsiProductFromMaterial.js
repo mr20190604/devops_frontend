fb46cc93c6333dbf42212586b82c85c429539ebc
caolei <ZKJH@ZKJH-YJY-2>
2021-07-08 14:20:45 +0800

    修改产品添加和编辑方式

--- flash-vue-admin/src/api/dsi/dsiProductFromMaterial.js
+++ flash-vue-admin/src/api/dsi/dsiProductFromMaterial.js
@@ -1,35 +1,52 @@
 import request from '@/utils/request'
+
 export default {
-    getList:function(productId) {
-        return request({
-            url: '/product/from/material/list',
-            method: 'get',
-            params:{
-              productId:productId
-            }
-        })
-    },
-    add:function(params) {
-        return request({
-            url: '/product/from/material',
-            method: 'post',
-            params
-        })
-    },
-    update:function(params) {
-        return request({
-            url: '/product/from/material',
-            method: 'PUT',
-            params
-        })
-    },
-    remove:function(id) {
-        return request({
-            url: '/product/from/material',
-            method: 'delete',
-            params: {
-                id: id
-            }
-        })
-    }
+  getList: function(productId) {
+    return request({
+      url: '/product/from/material/list',
+      method: 'get',
+      params: {
+        productId: productId
+      }
+    })
+  },
+  add: function(params) {
+    return request({
+      url: '/product/from/material',
+      method: 'post',
+      params
+    })
+  },
+  update: function(params) {
+    return request({
+      url: '/product/from/material',
+      method: 'PUT',
+      params
+    })
+  },
+  remove: function(id) {
+    return request({
+      url: '/product/from/material',
+      method: 'delete',
+      params: {
+        id: id
+      }
+    })
+  },
+  addList: function(list) {
+    return request({
+      url: '/product/from/material/addList',
+      method: 'post',
+      data: list
+    })
+  },
+  deleteList: function(productId) {
+    return request({
+      url: '/product/from/material/deleteList',
+      method: 'delete',
+      params: {
+        productId: productId
+      }
+    })
+  }
 }
